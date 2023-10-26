2679aae20b4a20b824141883fe2e3e6fe2861fb4
shadab-k <shadabpp27@gmail.com>
2023-10-26 16:03:49 +0530

    latest commit

--- src/utils/services.js
+++ src/utils/services.js
@@ -2,10 +2,7 @@ import axios from 'axios';
 const ENV_DATA = {
   BASE_URL: process.env.REACT_APP_BASE_URL,
 };
-// console.log(process.env)
-/**
- * create axios instance
- */
+
 const $http = axios.create({
     baseURL: ENV_DATA.BASE_URL,
 
